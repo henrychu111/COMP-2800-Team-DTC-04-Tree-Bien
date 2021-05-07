f0630b5532ac8d214b8075be97614be48b365c94
henrychu111 <henrychuuuu@gmail.com>
2021-05-07 01:39:57 -0700

    created google api

--- tree-bien/src/App.js
+++ tree-bien/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { GoogleMap, withScriptjs, withGoogleMap} from "react-google-maps";
+
+function Map() {
+  return (
+    <GoogleMap defaultZoom ={10} defaultCenter = {{lat: 49.263569, lng: -123.138573}}/>
+  )
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {{width: '100vw', height: "100vh"}}>
+      <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCZGmMsYQX8uffhtpk7WC68LCnO80EE6Vw`}
+        loadingElement = {<div style={{ height: "100%"}} />}
+        containerElement = {<div style={{ height: "100%"}} />}
+        mapElement = {<div style={{ height: "100%"}} />}
+      />
     </div>
-  );
+  )
+
 }
 
 export default App;
