dd6ffd1900ba2b8cd7a2a12f9b8102e1e04e498b
Paul Seong Uk Yeon <syeon@my.bcit.ca>
2021-05-11 13:42:54 -0700

    Add initial commit by editting app.js and firebase.js

---
+++ tree-bien/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBeSR1JaZlsI39VDoCJgVKteOM0e8guvIc",
+    authDomain: "tree-bien.firebaseapp.com",
+    projectId: "tree-bien",
+    storageBucket: "tree-bien.appspot.com",
+    messagingSenderId: "340858017271",
+    appId: "1:340858017271:web:ed4e5da341ae0d4a334af2"
+};
+// Initialize Firebase
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;
