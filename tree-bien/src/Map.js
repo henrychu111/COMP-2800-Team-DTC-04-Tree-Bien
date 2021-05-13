2fa67892689bec23c9e47205fba28912ffff71f6
henrychu111 <henrychuuuu@gmail.com>
2021-05-13 01:08:55 -0700

    connected to firestore and extracted data from it

--- tree-bien/src/Map.js
+++ tree-bien/src/Map.js
@@ -1,43 +1,82 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import GoogleMapReact from 'google-map-react';
-import * as parksData from "./data/parks.json";
-import {Popover} from 'antd';
+import { Popover } from 'antd';
 import 'antd/dist/antd.css';
-import {ShopOutlined} from '@ant-design/icons';
+import { ShopOutlined } from '@ant-design/icons';
+import firebase from './firebase';
 
 function Map() {
-    return (
-        <div style = {{width: '100vw', height: "100vh", margin:"0"}}>
-       
-      <GoogleMapReact bootstrapURLKeys = {{key :  process.env.REACT_APP_GOOGLE_MAP_KEY}} 
-      defaultZoom ={10} defaultCenter = {{lat: 49.263569, lng: -123.138573}}
+  const [plantingSites, setPlantingSites] = useState([]);
+  const [plantShops, setPlantShops] = useState([]);
+  const [location, setLocation] = useState({latitude: 49.263569, longitude: -123.138573})
+
+  useEffect(() => {
+    const fetchPlantingSites = async () => {
+      const response = firebase.db.collection("plantingsites");
+      const data = await response.get();
+      data.docs.forEach((site) => {
+        setPlantingSites([...plantingSites, { id: site.id, ...site.data() }])
+      })
+    }
+    fetchPlantingSites();
+  }, [])
+
+  useEffect(() => {
+    const fetchPlantingShops = async () => {
+      const response = firebase.db.collection("plantshops");
+      const data = await response.get();
+      data.docs.forEach((shop) => {
+        setPlantShops([...plantShops, { id: shop.id, ...shop.data() }])
+      })
+    }
+    fetchPlantingShops();
+  }, [])
+
+  useEffect(() => {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+    setLocation({latitude: position.coords.latitude, longitude: position.coords.longitude});
+  })
+}
+  }, [])
+  return (
+    <div style={{ width: '90vw', height: "80vh", margin: "50px auto" }}>
+        <div style={{marginBottom: '10px'}}>
+        <img style={{ width: '25px' }} src="/shopping-cart-marker.png" />
+        <span style={{marginRight: '20px'}}>= Tree Vendor</span>
+        <img style={{ width: '25px' }} src="/tree_map_icon.png" />
+        <span>= Planting Site</span>
+        </div>
+      <GoogleMapReact bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_KEY }}
+        defaultZoom={13} center={{ lat: location.latitude, lng: location.longitude}}
+        defaultCenter={{ lat: location.latitude, lng: location.longitude}}
       >
-       {parksData.plantshop.map((shop) =>{
-          return <Popover lat={shop.latitude} lng= {shop.longitude} key={shop.shop_id}
-                    content = {<div>{shop.address + ", " + shop.city} <br/>
-                                    {shop.province + ", " + shop.country + ", " + shop.postal_code}
-                                </div>}
-                    title = {<div><ShopOutlined style={{color:'green'}} />{" " + shop.name}</div>}
-                 >
-                    <img style={{width: '40px'}} src="/shopping-cart-marker.png"  />
-                </Popover>
-                
-            
-  })}
-        {parksData.plantingsite.map((site) =>{
-          return <Popover lat={site.latitude} lng= {site.longitude} key={site.site_id}
-                    content = {<div>{site.address + ", " + site.city}<br/>
-                                     {site.province + ", " + site.country + ", " + site.postal_code}
-                              </div>}
-                        title = {<div><img style={{width: "20px"}} src="/tree_icon.png"/>{" " + site.name}</div>}
-                >
-                     <img style={{width: '40px'}} src="/tree_map_icon.png" />
-                </Popover>
-  
-  })}
-      </GoogleMapReact> 
-      </div>
-    );
+        {plantShops && plantShops.map((shop) => {
+          return <Popover lat={shop.latitude} lng={shop.longitude} key={shop.id}
+            content={<div>{shop.address + ", " + shop.city} <br/>
+              {shop.province + ", " + shop.country + ", " + shop.postalcode}<br/>
+              <b> Specialty Trees: </b> {shop.specialties}
+            </div>}
+            title={<div><ShopOutlined style={{ color: 'green' }} />{" " + shop.name}</div>}
+          >
+            <img style={{ width: '40px' }} src="/shopping-cart-marker.png" />
+          </Popover>
+        })}
+        {plantingSites && plantingSites.map((site) => {
+          return <Popover lat={site.latitude} lng={site.longitude} key={site.id}
+            content={<div>{site.address + ", " + site.city}<br/>
+              {site.province + ", " + site.country + ", " + site.postalcode}<br/>
+              <b> Best Trees To Plant:</b> {site.suggestedplants}
+            </div>}
+            title={<div><img style={{ width: "20px" }} src="/tree_icon.png" />{" " + site.name}</div>}
+          >
+            <img style={{ width: '40px' }} src="/tree_map_icon.png" />
+          </Popover>
+
+        })}
+      </GoogleMapReact>
+    </div>
+  );
 }
 
 export default Map
