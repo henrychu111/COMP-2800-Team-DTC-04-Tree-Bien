8aa0919e2c7bc5aa5e813c86495d24a9fac8134d
April Cheng <april.sh.cheng@gmail.com>
2021-05-12 22:24:19 -0700

    Revised AddTree.js ShowTreeData.js TreeForm.js and added myTreePage.js

---
+++ tree-bien/src/components/AddTree/ShowTreeData.js
@@ -0,0 +1,141 @@
+import React from "react";
+import "../../css/ShowTreeData.css";
+import treeImage from "../../images/tree-shadow.png";
+// import { render } from "@testing-library/react";
+import UpdateTreeData from "./UpdateTreeData"; //Fix during integration
+import ListGroup from "react-bootstrap/ListGroup";
+
+//https://www.carlrippon.com/formatting-dates-and-numbers-in-react/
+function formatDate(change_date) {
+  console.log(change_date);
+  return new Intl.DateTimeFormat("en-GB", {
+    year: "2-digit",
+    month: "long",
+    day: "2-digit",
+  }).format(Date.parse(change_date));
+}
+
+class ShowTreeData extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showPopup: false,
+    };
+  }
+
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup,
+      dictKey: null,
+      value: null,
+    });
+  }
+
+  makeTogglePopup = (dictKey, value) => {
+    return () => {
+      this.setState({
+        showPopup: !this.state.showPopup,
+        dictKey: dictKey,
+        value: value,
+      });
+    };
+  };
+
+  render() {
+    return (
+      <div className="display-tree-info">
+        <div>
+          <img
+            src={treeImage}
+            alt="tree-shadow"
+            id="tree-page-tree-image"
+          ></img>
+        </div>
+        <div id="tree-page-photo-album">Add Tree Image</div>
+        <div className="display-item-details">
+          <ListGroup>
+            <ListGroup.Item
+              className="info-item"
+              onClick={this.makeTogglePopup.bind(this)(
+                "name",
+                this.props.tree.name
+              )}
+            >
+              {" "}
+              Name:
+              <b> {this.props.tree.name}</b>
+            </ListGroup.Item>
+
+            <ListGroup.Item
+              className="info-item"
+              onClick={this.makeTogglePopup.bind(this)(
+                "gender",
+                this.props.tree.gender
+              )}
+              variant="primary"
+            >
+              Gender:
+              <b> {this.props.tree.gender}</b>
+            </ListGroup.Item>
+
+            <ListGroup.Item
+              className="info-item"
+              onClick={this.makeTogglePopup.bind(this)(
+                "height",
+                this.props.tree.height
+              )}
+              variant="secondary"
+            >
+              Height (cm):
+              <b> {this.props.tree.height}</b>
+            </ListGroup.Item>
+
+            <ListGroup.Item
+              className="info-item"
+              onClick={this.makeTogglePopup.bind(this)(
+                "birthday",
+                this.props.tree.birthday
+              )}
+              variant="success"
+            >
+              Birthday:
+              <b> {formatDate(this.props.tree.birthday)}</b>
+            </ListGroup.Item>
+            <ListGroup.Item
+              className="info-item"
+              onClick={this.makeTogglePopup.bind(this)(
+                "specie",
+                this.props.tree.specie
+              )}
+              variant="danger"
+            >
+              Specie:
+              <b> {this.props.tree.specie}</b>
+            </ListGroup.Item>
+            <ListGroup.Item
+              className="info-item"
+              onClick={this.makeTogglePopup.bind(this)(
+                "personality",
+                this.props.tree.personality
+              )}
+              variant="warning"
+            >
+              Personality:
+              <b> {this.props.tree.personality}</b>
+            </ListGroup.Item>
+          </ListGroup>
+
+          {this.state.showPopup ? (
+            <UpdateTreeData
+              closePopup={this.togglePopup.bind(this)}
+              dictKey={this.state.dictKey}
+              value={this.state.value}
+            />
+          ) : null}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ShowTreeData;
---
+++ tree-bien/src/components/AddTree/TreeForm.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+// import ReactDom from 'react-dom';
+import { db } from "../../firebase";
+import "../../css/TreeForm.css";
+
+const TreeForm = (props) => {
+  // console.log(props);
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+  const [height, setHeight] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const [specie, setSpecie] = useState("");
+
+  const [personality, setPersonality] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    db.collection("add-new-tree")
+      .doc("New-Tree") //added new document named New-Tree
+      .set({
+        name: name,
+        gender: gender,
+        height: height + "cm",
+        birthday: birthday,
+        specie: specie,
+        personality: personality,
+      })
+      .then(() => {
+        alert("Form submitted");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+
+    setName("");
+    setGender("");
+    setHeight("");
+    setBirthday("");
+    setSpecie("");
+    setPersonality("");
+    props.closePopup();
+  };
+
+  return (
+    <div className="popup">
+      <div className="popup_inner">
+        <button className="form-close-button" onClick={props.closePopup}>
+          x
+        </button>
+
+        <form className="add_tree_form" onSubmit={handleSubmit}>
+          <h3 className="form-title">Add New Tree Info</h3>
+          <input
+            type="text"
+            placeholder="Name"
+            className="form_input"
+            value={name}
+            onChange={(input) => setName(input.target.value)}
+          />
+          <br></br>
+          <input
+            type="text"
+            placeholder="Gender"
+            className="form_input"
+            value={gender}
+            onChange={(input) => setGender(input.target.value)}
+          />
+          <br></br>
+          <input
+            type="number"
+            placeholder="Height (cm)"
+            className="form_input"
+            value={height}
+            onChange={(input) => setHeight(input.target.value)}
+          />
+          <br></br>
+          <label id="bday-label">Birthday</label>
+          <input
+            type="date"
+            placeholder="Birthday"
+            className="form_input"
+            id="b-day"
+            value={birthday}
+            onChange={(input) => setBirthday(input.target.value)}
+          />
+          <br></br>
+          <input
+            type="text"
+            placeholder="Specie"
+            className="form_input"
+            value={specie}
+            onChange={(input) => setSpecie(input.target.value)}
+          />
+          <br></br>
+          <input
+            type="text"
+            placeholder="Personality"
+            className="form_input"
+            value={personality}
+            onChange={(input) => setPersonality(input.target.value)}
+          />
+          <br></br>
+          <button type="submit" class="form_submit_button">
+            Set Up Tree!
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default TreeForm;
