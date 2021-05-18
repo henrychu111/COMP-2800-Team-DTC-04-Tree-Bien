874e970b934b60a6ad1727dbd0a87dffbe34bbdb
Paul Seong Uk Yeon <syeon@my.bcit.ca>
2021-05-18 14:52:15 -0700

    implement easter egg to about us page

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,28 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import Paul from "./images/AvatarMaker_Paul.png";
 import April from "./images/AvatarMaker_April.png";
 import Hannah from "./images/AvatarMaker_Hannah.png";
 import Henry from "./images/AvatarMaker_Henry.png";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import Chris from "./images/AvatarMaker_Chris.png";
+import Patrick from "./images/AvatarMaker_Patrick.png";
+import Shachi from "./images/AvatarMaker_Shachi.png";
+import Hoda from "./images/AvatarMaker_Hoda.png";
+
 import "./AboutUs.css";
 
 
 const AboutUsPage = () => {
+ 
+  const [paulphoto, setPaul] = useState(true);
+  const [hannahphoto, setHannah] = useState(true);
+  const [henryphoto, setHenry] = useState(true);
+  const [aprilphoto, setApril] = useState(true);
+
+
+  const changeInfo = (person) => {
+
+  }
+
   return (
     <div id="about-us-div">
       <Jumbotron className="about-us-jumbo" fluid>
         <div className="about-us-culture">
           <h1>
-            Tree Bien
+            Tree Bien <br></br>
+            {!paulphoto && !hannahphoto && !henryphoto && !aprilphoto ? ("Super Team") : null}
           </h1>
           <div id="about-us-description">
             <p>
-              Our team is passionate about the environment and believe that with our
-              efforts, our society will be influenced to foster a better relationship
-              with mother nature. At Tree Bien, we believe each of our different
-              strengths can create an environment where each team member can ask,
-              learn, and teach one another. Root for trees, as they are humanity’s
-              lifeline.
+              {!paulphoto && !hannahphoto && !henryphoto && !aprilphoto ? 
+              ("These are the supervisors that guided us to our success! Thank you to each and every one of you! " + 
+              "From your favourite students!") 
+              : ("Our team is passionate about the environment and believe that with our efforts, " + 
+                "our society will be influenced to foster a better relationship with mother nature." + 
+                " At Tree Bien, we believe each of our different strengths can create an environment where " + 
+                "each team member can ask, learn, and teach one another. Root for trees, as they are humanity’s lifeline.")}
             </p>
           </div>
         </div>
@@ -30,24 +48,36 @@ const AboutUsPage = () => {
 
       <div id="founder-photo-container">
         <div className="people-picture" id="paul-info">
-          <img src={Paul}></img>
-          <p className="founders-name">Paul Yeon</p>
-          <p className="founders-title">Product Manager</p>
+          <img 
+          src={paulphoto ? Paul : Chris} 
+          onClick={() => setPaul(!paulphoto)}
+          />
+          <p className="founders-name">{paulphoto ? ("Paul Yeon") : ("Chris Thompson")}</p>
+          <p className="founders-title">{paulphoto ? ("Product Manager") : ("DTC Project Lead Instructor")}</p>
         </div>
         <div className="people-picture" id="hannah-info">
-          <img src={Hannah}></img>
-          <p className="founders-name">Hannah Kim</p>
-          <p className="founders-title">UX/UI Designer</p>
+          <img
+          src={hannahphoto ? Hannah : Patrick} 
+          onClick={() => setHannah(!hannahphoto)} 
+          />
+          <p className="founders-name">{hannahphoto ? ("Hannah Kim") : ("Patrick Guichon")}</p>
+          <p className="founders-title">{hannahphoto ? ("UX/UI Designer") : ("Project Team Supervisor")}</p>
         </div>
         <div className="people-picture" id="henry-info">
-          <img src={Henry}></img>
-          <p className="founders-name">Henry Chu</p>
-          <p className="founders-title">Lead Developer</p>
+          <img 
+          src={henryphoto ? Henry : Shachi} 
+          onClick={() => setHenry(!henryphoto)}
+          />
+          <p className="founders-name">{henryphoto ? ("Henry Chu") : ("Shachi Singh")}</p>
+          <p className="founders-title">{henryphoto ? ("Lead Developer") : ("Project Team Supervisor")}</p>
         </div>
         <div className="people-picture" id="april-info">
-          <img src={April}></img>
-          <p className="founders-name">April Cheng</p>
-          <p className="founders-title" id="april-title">Developer</p>
+          <img
+          src={aprilphoto ? April : Hoda}
+          onClick={() => setApril(!aprilphoto)}
+          />
+          <p className="founders-name">{aprilphoto ? ("April Cheng") : ("Hoda Rashedi")}</p>
+          <p className="founders-title" id="april-title">{aprilphoto ? ("Developer") : ("Project Team Supervisor")}</p>
         </div>
       </div>
     </div>
